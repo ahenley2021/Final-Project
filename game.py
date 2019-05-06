8551df9d3fb86fa5acea86493ffa60783c17eb7c
ashenley <33043435+ashenley@users.noreply.github.com>
2019-05-06 13:02:07 -0400

    finished adding import statements

--- game.py
+++ game.py
@@ -12,14 +12,17 @@ FPS = 10
 rooms = pygame.sprite.Group()
 
 gardener = Character()
-tLeft = Room(10, 0)
-tMid = Room(100, 0)
-tRight = Room(245, 0)
+tLeft = Room(10, 0, 80, 35)
+tMid = Room(100, 0, 225, 40)
+tRight = Room(245, 0, 310, 25)
 
-rTop = Room(296, 65)
-rBottom = Room(335, 200)
+rTop = Room(296, 654, 400, 171)
+rBottom = Room(335, 200, 400, 250)
 
-bRight = Room(150, 265)
-bLeft = Room(0, 230)
+bRight = Room(150, 265, 240, 300)
+bLeft = Room(0, 230, 70, 300)
 
-lLeft = Room(0, 80)
+left = Room(0, 80, 45, 125)
+
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption("Clue")
--- room.py
+++ room.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.locals import *
 
 class Room(pygame.sprite.Sprite):
-    def __init__(self, topLeft, bottomRight):
+    def __init__(self, topLeftX, topLeftY, bottomRightX, bottomRightY):
         super().__init__()
-        self.x = topLeft
-        self.y = bottomRight
+        self.x = topLeftX
+        self.y = topLeftY
+        self.rect = pygame.Rect(self.x, self.y, bottomRightX, bottomRightY)
